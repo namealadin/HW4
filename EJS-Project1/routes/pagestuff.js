929118b6ecf255a653bf1aaaa19466be524f63df
ALADIN <aladin@aladin.99>
2020-05-05 08:01:30 -0700

    Last modification

--- EJS-Project1/routes/pagestuff.js
+++ EJS-Project1/routes/pagestuff.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('This page is added by me');
+  //res.send('This page is added by me');
+  res.render('mypage', { title: 'This my page' });
+});
+
+/* GET users listing. */
+router.get('/help', function(req, res, next) {
+  //res.send('This page is added by me');
+  res.render('mypagehelp', { title: 'This my page' });
 });
 
 module.exports = router;
